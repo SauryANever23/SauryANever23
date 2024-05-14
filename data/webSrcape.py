61ea216377cf8631dc46e0d0a6ab834d301346de
Saurya Jha <sauryajha9841@gmail.com>
2024-05-14 17:17:28 +0545

    removing

--- data/webSrcape.py
+++ data/webSrcape.py
@@ -6,10 +6,10 @@ from bs4 import BeautifulSoup
 
 url = "https://www.amazon.in/s?k=laptops&crid=2WOERC8BUXQLT&sprefix=laptop%2Caps%2C343&ref=nb_sb_noss_1"
 
-headers = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.95 Safari/537.36'}
 
 
-r = requests.get(url, headers=headers)
+
+r = requests.get(url)
 # Whenever using BeautifulSoup create a soup object    
 soup = BeautifulSoup(r.text, 'html.parser')
 print(soup.prettify())
