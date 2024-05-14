72c05b8bbaa1b695509a413465e1b31cb4d06b64
Saurya Jha <sauryajha9841@gmail.com>
2024-05-14 17:14:59 +0545

    creating the main file for webscraping

---
+++ data/webSrcape.py
@@ -0,0 +1,12 @@
+import requests
+from bs4 import BeautifulSoup
+
+
+
+
+url = "https://www.amazon.in/s?k=laptops&crid=2WOERC8BUXQLT&sprefix=laptop%2Caps%2C343&ref=nb_sb_noss_1"
+
+r = requests.get(url)
+# Whenever using BeautifulSoup create a soup object    
+soup = BeautifulSoup(r.text, 'html.parser')
+print(soup.prettify())
