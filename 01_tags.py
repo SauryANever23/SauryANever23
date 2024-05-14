1b79412bd56a51399d1dfd0b42a2936489f45ea1
Saurya Jha <sauryajha9841@gmail.com>
2024-05-14 17:03:45 +0545

    making and tesing the files

--- 01_tags.py
+++ 01_tags.py
@@ -30,10 +30,6 @@ soup = BeautifulSoup(html_doc, 'html.parser')
 # for parent in soup.find(class_="box").parents:
 #     print(parent)
 
-# count = soup.find(class_="container")
-# count.name = "span"
-# count["class"] = "ucontainer"
-# print(count)
 
 
 # for child in soup.find(class_="ucontainer").children:
@@ -43,4 +39,36 @@ soup = BeautifulSoup(html_doc, 'html.parser')
 #     print(parent)
 #     break
 
+# count = soup.find(class_="container")
+# count.name = "span"
+# count["class"] = ""
+# print(count)
 
+
+
+#* ul = soup.new_tag("ul")
+#*li = soup.new_tag("li")
+#* li.string = "helol"
+#* ul.append(li)
+
+#* li = soup.new_tag("li")
+#* li.string = "about"
+#* ul.append(li)
+
+#* soup.html.body.insert(0, ul)
+#* with open("modified.html", "w") as f:
+#*    f.write(str(soup))
+
+
+# cont = soup.find(class_="container")
+# print(cont.has_attr('class'))
+
+def has_class_no_id(tag):
+    return not tag.has_attr("class") and not tag.has_attr("id")
+
+def has_content(tag):
+    return tag.has_attr("content")
+
+reult = soup.find_all(has_content)
+print(reult)
+results = soup.find_all(has_class_no_id)
