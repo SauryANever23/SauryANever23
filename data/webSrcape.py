7ec9b316d9d03bd08cde4a499c1403d075bb2b7a
Saurya Jha <sauryajha9841@gmail.com>
2024-05-16 18:16:30 +0545

    ..

--- data/webSrcape.py
+++ data/webSrcape.py
@@ -2,19 +2,9 @@ import requests
 from bs4 import BeautifulSoup
 
 url = "https://www.amazon.in/s?k=laptops&crid=2WOERC8BUXQLT&sprefix=laptop%2Caps%2C343&ref=nb_sb_noss_1"
+headers = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.95 Safari/537.36'}
 
-r = requests.get(url)
+r = requests.get(url, headers=headers)
 soup = BeautifulSoup(r.text, 'html.parser')
 
-# getting titles from the website
-span = soup.select("class.a-size-mini a-spacing-none a-color-base s-line-clamp-2")
-
-for spans in span:
-    print(spans.string, "\n\n")
-
-print(span, "\n\n")
-# scraping price
-# prices = soup.select("span.a-price-whole")
-
-# for price in prices:
-#     print(price.string, "\n\n")
+print(soup.prettify())
