510ba5a535c81525a56b27c8c68d9a6e382c397a
Saurya Jha <sauryajha9841@gmail.com>
2024-05-16 18:20:02 +0545

    ..

--- data/webSrcape.py
+++ data/webSrcape.py
@@ -1,7 +1,7 @@
 import requests
 from bs4 import BeautifulSoup
 
-url = "https://www.amazon.in/s?k=laptops&crid=2WOERC8BUXQLT&sprefix=laptop%2Caps%2C343&ref=nb_sb_noss_1"
+url = "https://www.amazon.in/s?k=laptop&crid=3K3RQ3DZ80PCB&sprefix=laptop%2Caps%2C250&ref=nb_sb_noss_1"
 headers = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.95 Safari/537.36'}
 
 r = requests.get(url, headers=headers)
