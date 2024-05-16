88bc39e907b8f2b502b40a1faaff2baecb8ebfad
Saurya Jha <sauryajha9841@gmail.com>
2024-05-16 18:33:24 +0545

    getting to scrape a random website

--- data/webSrcape.py
+++ data/webSrcape.py
@@ -1,10 +1,19 @@
 import requests
 from bs4 import BeautifulSoup
 
-url = "https://www.amazon.in/s?k=laptop&crid=3K3RQ3DZ80PCB&sprefix=laptop%2Caps%2C250&ref=nb_sb_noss_1"
+
+url = "https://wikileaks.org/+-Global-Economy-+.html"
 headers = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.95 Safari/537.36'}
 
 r = requests.get(url, headers=headers)
 soup = BeautifulSoup(r.text, 'html.parser')
 
-print(soup.prettify())
+# print(soup.prettify())
+
+titles = soup.select("h2.title")
+for title in titles:
+    print(title.string)
+
+images = soup.select("img.spip_logos")
+for image in images:
+    print(image['src'])
